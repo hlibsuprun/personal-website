502a13adf2eec418dc9b3c3eb6693c7a4baba082
Hlib Suprun <hlibsuprun@gmail.com>
2023-12-01 22:41:35 +0100

    init: start personal website

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <main>
+      <Head>
+        <title>Hlib Suprun</title>
+        <meta property="og:title" content="Hlib Suprun" key="title" />
+      </Head>
+    </main>
+  )
+}
